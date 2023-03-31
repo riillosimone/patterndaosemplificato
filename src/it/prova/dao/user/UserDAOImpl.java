052fa1a2dad4b1ce7dc59ead0c7aa26a41a52d59
riillosimone <riillosimone@gmail.com>
2023-03-31 14:50:57 +0200

    Commit di creazione progetto Alberto

---
+++ src/it/prova/dao/user/UserDAOImpl.java
@@ -0,0 +1,345 @@
+package it.prova.dao.user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.prova.dao.AbstractMySQLDAO;
+import it.prova.model.User;
+
+public class UserDAOImpl extends AbstractMySQLDAO implements UserDAO {
+
+	// la connection stavolta fa parte del this, quindi deve essere 'iniettata'
+	// dall'esterno
+	public UserDAOImpl(Connection connection) {
+		super(connection);
+	}
+
+	@Override
+	public List<User> list() throws Exception {
+		// prima di tutto cerchiamo di capire se possiamo effettuare le operazioni
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		ArrayList<User> result = new ArrayList<User>();
+
+		try (Statement ps = connection.createStatement(); ResultSet rs = ps.executeQuery("select * from user")) {
+
+			while (rs.next()) {
+				User userTemp = new User();
+				userTemp.setNome(rs.getString("NOME"));
+				userTemp.setCognome(rs.getString("COGNOME"));
+				userTemp.setLogin(rs.getString("LOGIN"));
+				userTemp.setPassword(rs.getString("PASSWORD"));
+				userTemp.setDateCreated(
+						rs.getDate("DATECREATED") != null ? rs.getDate("DATECREATED").toLocalDate() : null);
+				userTemp.setId(rs.getLong("ID"));
+				result.add(userTemp);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+	}
+
+	@Override
+	public User get(Long idInput) throws Exception {
+		// prima di tutto cerchiamo di capire se possiamo effettuare le operazioni
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (idInput == null || idInput < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		User result = null;
+		try (PreparedStatement ps = connection.prepareStatement("select * from user where id=?")) {
+
+			ps.setLong(1, idInput);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					result = new User();
+					result.setNome(rs.getString("NOME"));
+					result.setCognome(rs.getString("COGNOME"));
+					result.setLogin(rs.getString("LOGIN"));
+					result.setPassword(rs.getString("PASSWORD"));
+					result.setDateCreated(
+							rs.getDate("DATECREATED") != null ? rs.getDate("DATECREATED").toLocalDate() : null);
+					result.setId(rs.getLong("ID"));
+				} else {
+					result = null;
+				}
+			} // niente catch qui
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+	}
+
+	@Override
+	public int insert(User utenteInput) throws Exception {
+		// prima di tutto cerchiamo di capire se possiamo effettuare le operazioni
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (utenteInput == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"INSERT INTO user (nome, cognome, login, password, dateCreated) VALUES (?, ?, ?, ?, ?);")) {
+			ps.setString(1, utenteInput.getNome());
+			ps.setString(2, utenteInput.getCognome());
+			ps.setString(3, utenteInput.getLogin());
+			ps.setString(4, utenteInput.getPassword());
+			// quando si fa il setDate serve un tipo java.sql.Date
+			ps.setDate(5, java.sql.Date.valueOf(utenteInput.getDateCreated()));
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+	}
+
+	@Override
+	public int update(User utenteInput) throws Exception {
+		// prima di tutto cerchiamo di capire se possiamo effettuare le operazioni
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (utenteInput == null || utenteInput.getId() == null || utenteInput.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement(
+				"UPDATE user SET nome=?, cognome=?, login=?, password=?, dateCreated=? where id=?;")) {
+			ps.setString(1, utenteInput.getNome());
+			ps.setString(2, utenteInput.getCognome());
+			ps.setString(3, utenteInput.getLogin());
+			ps.setString(4, utenteInput.getPassword());
+			// quando si fa il setDate serve un tipo java.sql.Date
+			ps.setDate(5, java.sql.Date.valueOf(utenteInput.getDateCreated()));
+			ps.setLong(6, utenteInput.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+	}
+
+	@Override
+	public int delete(User utenteInput) throws Exception {
+		// prima di tutto cerchiamo di capire se possiamo effettuare le operazioni
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (utenteInput == null || utenteInput.getId() == null || utenteInput.getId() < 1)
+			throw new Exception("Valore di input non ammesso.");
+
+		int result = 0;
+		try (PreparedStatement ps = connection.prepareStatement("DELETE FROM user WHERE ID=?")) {
+			ps.setLong(1, utenteInput.getId());
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+	}
+
+	@Override
+	public List<User> findAllWhereDateCreatedGreaterThan(LocalDate dateCreatedInput) throws Exception {
+		// prima di tutto cerchiamo di capire se possiamo effettuare le operazioni
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+
+		if (dateCreatedInput == null)
+			throw new Exception("Valore di input non ammesso.");
+
+		ArrayList<User> result = new ArrayList<User>();
+
+		try (PreparedStatement ps = connection.prepareStatement("select * from user where dateCreated > ? ;")) {
+			// quando si fa il setDate serve un tipo java.sql.Date
+			ps.setDate(1, java.sql.Date.valueOf(dateCreatedInput));
+
+			try (ResultSet rs = ps.executeQuery();) {
+				while (rs.next()) {
+					User userTemp = new User();
+					userTemp.setNome(rs.getString("NOME"));
+					userTemp.setCognome(rs.getString("COGNOME"));
+					userTemp.setLogin(rs.getString("LOGIN"));
+					userTemp.setPassword(rs.getString("PASSWORD"));
+					userTemp.setDateCreated(
+							rs.getDate("DATECREATED") != null ? rs.getDate("DATECREATED").toLocalDate() : null);
+					userTemp.setId(rs.getLong("ID"));
+					result.add(userTemp);
+				}
+			} // niente catch qui
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+	}
+
+	// DA FARE PER ESERCIZIO
+
+	@Override
+	public List<User> findAllByCognome(String cognomeInput) throws Exception {
+		// prima di tutto cerchiamo di capire se possiamo effettuare le operazioni
+		if (isNotActive())
+			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+		if (cognomeInput == null || cognomeInput.isBlank()) {
+			throw new Exception("Valore di input non ammesso.");
+		}
+		List<User> result = new ArrayList<>();
+		try (PreparedStatement ps = connection.prepareStatement("select * from user where cognome like ? ;")) {
+			ps.setString(1,cognomeInput);
+
+			try (ResultSet rs = ps.executeQuery();) {
+				while (rs.next()) {
+					User userTemp = new User();
+					userTemp.setNome(rs.getString("NOME"));
+					userTemp.setCognome(rs.getString("COGNOME"));
+					userTemp.setLogin(rs.getString("LOGIN"));
+					userTemp.setPassword(rs.getString("PASSWORD"));
+					userTemp.setDateCreated(
+							rs.getDate("DATECREATED") != null ? rs.getDate("DATECREATED").toLocalDate() : null);
+					userTemp.setId(rs.getLong("ID"));
+					result.add(userTemp);
+				}
+			} // niente catch qui
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return result;
+	}
+
+	@Override
+	public List<User> findAllByLoginIniziaCon(String caratteriInizialiInput) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findByLoginAndPassword(String loginInput, String passwordInput) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findAllByPasswordIsNull() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findByExample(User input) throws Exception {
+		// TODO Auto-generated method stub
+		return new ArrayList<>();
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// ############################################################
+	// ############ SOLUZIONE FINDBYEXAMPLE #######################
+	// ############################################################
+//	public List<User> findByExampleSolution(User example) throws Exception {
+//		// prima di tutto cerchiamo di capire se possiamo effettuare le operazioni
+//		if (isNotActive())
+//			throw new Exception("Connessione non attiva. Impossibile effettuare operazioni DAO.");
+//
+//		if (example == null)
+//			throw new Exception("Valore di input non ammesso.");
+//
+//		ArrayList<User> result = new ArrayList<User>();
+//		User userTemp = null;
+//
+//		String query = "select * from user where 1=1 ";
+//		if (example.getCognome() != null && !example.getCognome().isEmpty()) {
+//			query += " and cognome like '" + example.getCognome() + "%' ";
+//		}
+//		if (example.getNome() != null && !example.getNome().isEmpty()) {
+//			query += " and nome like '" + example.getNome() + "%' ";
+//		}
+//
+//		if (example.getLogin() != null && !example.getLogin().isEmpty()) {
+//			query += " and login like '" + example.getLogin() + "%' ";
+//		}
+//
+//		if (example.getPassword() != null && !example.getPassword().isEmpty()) {
+//			query += " and password like '" + example.getPassword() + "%' ";
+//		}
+//
+//		if (example.getDateCreated() != null) {
+//			query += " and DATECREATED='" + java.sql.Date.valueOf(example.getDateCreated()) + "' ";
+//		}
+//
+//		try (Statement ps = connection.createStatement()) {
+//			ResultSet rs = ps.executeQuery(query);
+//
+//			while (rs.next()) {
+//				userTemp = new User();
+//				userTemp.setNome(rs.getString("NOME"));
+//				userTemp.setCognome(rs.getString("COGNOME"));
+//				userTemp.setLogin(rs.getString("LOGIN"));
+//				userTemp.setPassword(rs.getString("PASSWORD"));
+//				userTemp.setDateCreated(
+//						rs.getDate("DATECREATED") != null ? rs.getDate("DATECREATED").toLocalDate() : null);
+//				userTemp.setId(rs.getLong("ID"));
+//				result.add(userTemp);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			throw e;
+//		}
+//		return result;
+//	}
+
+}
---
+++ src/it/prova/test/TestUser.java
@@ -0,0 +1,133 @@
+package it.prova.test;
+
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.List;
+
+import it.prova.connection.MyConnection;
+import it.prova.dao.Constants;
+import it.prova.dao.user.UserDAO;
+import it.prova.dao.user.UserDAOImpl;
+import it.prova.model.User;
+
+public class TestUser {
+
+	public static void main(String[] args) {
+
+		UserDAO userDAOInstance = null;
+
+		// ##############################################################################################################
+		// Grande novità: la Connection viene allestista dal chiamante!!! Non è più a
+		// carico dei singoli metodi DAO!!!
+		// ##############################################################################################################
+		try (Connection connection = MyConnection.getConnection(Constants.DRIVER_NAME, Constants.CONNECTION_URL)) {
+			// ecco chi 'inietta' la connection: il chiamante
+			userDAOInstance = new UserDAOImpl(connection);
+
+			System.out.println("In tabella user ci sono " + userDAOInstance.list().size() + " elementi.");
+
+			testInsertUser(userDAOInstance);
+			System.out.println("In tabella user ci sono " + userDAOInstance.list().size() + " elementi.");
+
+			testFindById(userDAOInstance);
+
+			testDeleteUser(userDAOInstance);
+			System.out.println("In tabella user ci sono " + userDAOInstance.list().size() + " elementi.");
+
+			testFindAllWhereDateCreatedGreaterThan(userDAOInstance);
+			System.out.println("In tabella user ci sono " + userDAOInstance.list().size() + " elementi.");
+
+			// ESERCIZIO SUCCESSIVO: implementare metodi mancanti nel DAO
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void testInsertUser(UserDAO userDAOInstance) throws Exception {
+		System.out.println(".......testInsertUser inizio.............");
+		int quantiElementiInseriti = userDAOInstance
+				.insert(new User("pluto", "plutotto", "ppp@example.com", "password@01", LocalDate.now()));
+		if (quantiElementiInseriti < 1)
+			throw new RuntimeException("testInsertUser : FAILED");
+
+		System.out.println(".......testInsertUser fine: PASSED.............");
+	}
+
+	private static void testFindById(UserDAO userDAOInstance) throws Exception {
+		System.out.println(".......testFindById inizio.............");
+		List<User> elencoVociPresenti = userDAOInstance.list();
+		if (elencoVociPresenti.size() < 1)
+			throw new RuntimeException("testFindById : FAILED, non ci sono voci sul DB");
+
+		User primoDellaLista = elencoVociPresenti.get(0);
+
+		User elementoCheRicercoColDAO = userDAOInstance.get(primoDellaLista.getId());
+		if (elementoCheRicercoColDAO == null || !elementoCheRicercoColDAO.getLogin().equals(primoDellaLista.getLogin()))
+			throw new RuntimeException("testFindById : FAILED, le login non corrispondono");
+
+		System.out.println(".......testFindById fine: PASSED.............");
+	}
+
+	private static void testDeleteUser(UserDAO userDAOInstance) throws Exception {
+		System.out.println(".......testDeleteUser inizio.............");
+		// me ne creo uno al volo
+		int quantiElementiInseriti = userDAOInstance
+				.insert(new User("Giuseppe", "Verdi", "g.verdi@example.com", "password@01", LocalDate.now()));
+		if (quantiElementiInseriti < 1)
+			throw new RuntimeException("testDeleteUser : FAILED, user da rimuovere non inserito");
+
+		List<User> elencoVociPresenti = userDAOInstance.list();
+		int numeroElementiPresentiPrimaDellaRimozione = elencoVociPresenti.size();
+		if (numeroElementiPresentiPrimaDellaRimozione < 1)
+			throw new RuntimeException("testDeleteUser : FAILED, non ci sono voci sul DB");
+
+		User ultimoDellaLista = elencoVociPresenti.get(numeroElementiPresentiPrimaDellaRimozione - 1);
+		userDAOInstance.delete(ultimoDellaLista);
+
+		// ricarico per vedere se sono scalati di una unità
+		int numeroElementiPresentiDopoDellaRimozione = userDAOInstance.list().size();
+		if (numeroElementiPresentiDopoDellaRimozione != numeroElementiPresentiPrimaDellaRimozione - 1)
+			throw new RuntimeException("testDeleteUser : FAILED, la rimozione non è avvenuta");
+
+		System.out.println(".......testDeleteUser fine: PASSED.............");
+	}
+
+	private static void testFindAllWhereDateCreatedGreaterThan(UserDAO userDAOInstance) throws Exception {
+		System.out.println(".......testFindAllWhereDateCreatedGreaterThan inizio.............");
+
+		LocalDate dataCreazione = LocalDate.parse("2022-02-02");
+		LocalDate dataCreazioneIlGiornoPrima = LocalDate.parse("2022-01-02");
+
+		// me ne creo un paio che fanno al caso mio così almeno due li troverò
+		User marioRossi = new User("Mario", "Rossi", "m.rossi@example.com", "password@01", dataCreazione);
+		User giuseppeBianchi = new User("Giuseppe", "Bianchi", "g.bianchi@example.com", "password@01", dataCreazione);
+
+		int quantiElementiInseriti = userDAOInstance.insert(marioRossi);
+		if (quantiElementiInseriti < 1)
+			throw new RuntimeException("testFindAllWhereDateCreatedGreaterThan : FAILED, user non inserito");
+
+		quantiElementiInseriti = userDAOInstance.insert(giuseppeBianchi);
+		if (quantiElementiInseriti < 1)
+			throw new RuntimeException("testFindAllWhereDateCreatedGreaterThan : FAILED, user non inserito");
+
+		// ora provo ad estrarli e devono avere tutti data successiva a quella scelta
+		List<User> elencoVociCreateDopoDataScelta = userDAOInstance
+				.findAllWhereDateCreatedGreaterThan(dataCreazioneIlGiornoPrima);
+		for (User userItem : elencoVociCreateDopoDataScelta) {
+			if (userItem.getDateCreated().isBefore(dataCreazioneIlGiornoPrima))
+				throw new RuntimeException(
+						"testFindAllWhereDateCreatedGreaterThan : FAILED, user con data precedente con id: "
+								+ userItem.getId());
+		}
+
+		System.out.println(".......testFindAllWhereDateCreatedGreaterThan fine: PASSED.............");
+	}
+	private static void testFindAllByCognome (UserDAO userDAOInstance) {
+		System.out.println(".......testFindAllByCognome inizio.............");
+		
+		
+	}
+
+}
